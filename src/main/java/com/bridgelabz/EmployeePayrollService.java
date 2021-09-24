4f5b708fc2efe3d2a0a4ec491a6773908534e88c
SUDIPP22 <panja370@gmail.com>
2021-09-25 01:17:31 +0530

    Added : To create EmployeePlayRollService and to read and write the information using console

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,5 +1,9 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Purpose : To implement Employee Payroll Service System
  *
@@ -8,13 +12,42 @@ package com.bridgelabz;
  * @since 2021-09-24
  */
 public class EmployeePayrollService {
+    public List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {
+        employeePayrollList = new ArrayList<>();
+    }
 
     public static void main(String[] args) {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.printWelcomeMessage();
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
     }
 
+    /* This method is implementing Welcome Message */
     public void printWelcomeMessage() {
         System.out.println("Welcome To The Employee Payroll Service System");
+    }
+
+    /**
+     * Purpose : To read the information of Employees from the console
+     * @param consoleInputReader takes the information of employees
+     */
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID : ");
+        int id = consoleInputReader.nextInt();
+        consoleInputReader.nextLine();
+        System.out.println("Enter Employee Name : ");
+        String name = consoleInputReader.nextLine();
+        System.out.println("Enter Employee Salary : ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    /* This method is implementing to write the Employee Payroll to the console */
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrollList);
     }
 }
