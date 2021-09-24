cfc33a00cda717c61ce255c224da3060e845d861
SUDIPP22 <panja370@gmail.com>
2021-09-25 00:38:50 +0530

    Added : Welcome Message

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To implement Employee Payroll Service System
+ *
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-09-24
+ */
+public class EmployeePayrollService {
+
+    public static void main(String[] args) {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.printWelcomeMessage();
+    }
+
+    public void printWelcomeMessage() {
+        System.out.println("Welcome To The Employee Payroll Service System");
+    }
+}
